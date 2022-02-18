f868d8ffe8bd199dc63b3a489dfe18a59003c8e9
Marco <marco@notemaster.com.br>
2022-02-18 18:17:41 -0300

    teste

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const http = require("express")
+const path = require("path")
+
+const app = express()
+const router = express.Router()
+
+router.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname + "/pages/home.html"))
+})
+
+router.get("/contato", (req, res)=>{
+    res.sendFile(path.join(__dirname + "/pages/contato.html"))
+})
+
+app.use(router)
+
+app.listen(proces.env.PORT || 3333, ()=>{
+    console.log("Servidor Rodando")
+})
+
